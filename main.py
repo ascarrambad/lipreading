0a0fd110f0f7ca7a925b9a3ef691529a04466d81
ascarrambad <matteoriva@me.com>
2019-04-10 12:38:49 +0200

    - Final structure for main script (not final NN structure)

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 import os
 
 import Data
+import Model
 
 import numpy as np
+import tensorflow as tf
 
 ################################################################################
 #################################### SACRED ####################################
@@ -31,8 +33,9 @@ def cfg():
     Shuffle = 1
 
     ### NET SPECS
-    NetSpec = 'FLATFEAT_FC(OBSA)t128_DP_FC(OBSB)t128_ID(ATTADV)_ID(ATTMVD)_DP_MASKBATCH_LSTMt128'
-    AdvSpec = 'FCt100_FCt100'
+    # NetSpec = '*CONVLSTM!2-8_*DP_CONV16r!2-1_*MP!2-2_*DP_CONV32r!2-1_*MP!2-2_*FLATFEAT!3_FC128t-51t'
+    NetSpec = '*FLATFEAT!2-1_CONV32r!2-1_*MP!2-2_CONV64r!2-1_*MP!2-2_*DP_*FLATFEAT!3_*ORESHAPE_LSTM128t!0_FC51t'
+    AdvSpec = '*GRADFLIP(Reversal)_FC(DomainFC)64t-3t'
     AdvFactors = '1-1.0'
     ObservedGrads = '' #separate by _
 
@@ -45,6 +48,7 @@ def cfg():
     EarlyStoppingPatience = 30
 
     OutDir = 'TEST.outdir'
+    TensorboardDir = './tensorboard'
     Remark = ''
 
 ################################################################################
@@ -53,14 +57,16 @@ def cfg():
 
 @ex.automain
 def main(
-        # spk and data
+        # Speakers
         SourceSpeakers,TargetSpeakers,ExtraSpeakers,WordsPerSpeaker,
-        # net settings
-        BatchSize,MaxEpochs,StopCondition,EarlyStoppingPatience,Shuffle,
-        # extra loss settings
-        MmdFactors,AdvFactors,
-        # rest
-        ObservedGrads,OutDir,_config
+        # Data
+        VideoNorm,Shuffle,InitStd,
+        # NN settings
+        NetSpec,AdvSpec,
+        # Training settings
+        BatchSize,LearnRate,MaxEpochs,StopCondition,EarlyStoppingPatience,
+        # Extra settings
+        ObservedGrads,OutDir,TensorboardDir,_config
         ):
     print('Config directory is:',_config)
 
@@ -82,9 +88,9 @@ def main(
     test_type = Data.SetType('test')
 
     # Load data
-    train_data, _ = data_loader.load_data(train_type, WordsPerSpeaker)
-    valid_data, _ = data_loader.load_data(valid_type, WordsPerSpeaker)
-    test_data, feature_size = data_loader.load_data(test_type, WordsPerSpeaker)
+    train_data, _ = data_loader.load_data(train_type, WordsPerSpeaker, VideoNorm, add_channel=True)
+    valid_data, _ = data_loader.load_data(valid_type, WordsPerSpeaker, VideoNorm, add_channel=True)
+    test_data, feature_size = data_loader.load_data(test_type, WordsPerSpeaker, VideoNorm, add_channel=True)
 
     # Create source & target datasets for all domain types
     train_source_set = Data.Set(train_data['Source'], BatchSize, Shuffle)
@@ -98,11 +104,36 @@ def main(
     test_target_set = Data.Set(test_data['Target'], BatchSize, Shuffle)
     test_extra_set = Data.Set(test_data['Extra'], BatchSize, Shuffle)
 
-    # b1 = train_source_set.next_batch()
-    # b2 = train_target_set.next_batch()
-    # b3 = b1.concatenate(b2)
-    # import pdb; pdb.set_trace()  # breakpoint 6c7267e0 //
-    # print('ciao')
+    # Model Builder
+    import pdb; pdb.set_trace()  # breakpoint 0018a623 //
+    builder = Model.Builder(InitStd)
+
+    # data_type = tf.float32#tf.as_dtype(train_source_set.data_dtype)
+    # data_shape = (64, 50, 80, 40, 1)#train_source_set.data_shape
+
+    # target_type = tf.int64#tf.as_dtype(train_source_set.target_dtype)
+    # target_shape = (None)#train_source_set.target_shape
+
+    # Adding placeholders for data
+    builder.add_placeholder(train_source_set.data_dtype, train_source_set.data_shape, 'Sequences')
+    builder.add_placeholder(tf.int64, [None], 'SeqLengths')
+    builder.add_placeholder(train_source_set.target_dtype, train_source_set.target_shape, 'WordTrgs')
+    builder.add_placeholder(train_source_set.domain_dtype, train_source_set.domain_shape, 'DomainTrgs')
+    builder.add_placeholder(tf.float32, [], 'Lambda')
+    builder.add_placeholder(tf.bool, [], 'Training')
+
+    # Create network
+    builder.add_main_specification(NetSpec, 'Sequences', 'WordTrgs')
+    # builder.add_specification(AdvSpec, 'Adv/Input', 'DomainTrgs')
+    import pdb; pdb.set_trace()  # breakpoint 308fdf86 //
+    builder.build_model()
+
+    import pdb; pdb.set_trace()  # breakpoint 69079128 //
+    trainer = Model.Trainer(MaxEpochs, LearnRate, builder.graph_specs, builder.placeholders, TensorboardDir)
+    trainer.init_session()
+    trainer.train([train_source_set], [valid_source_set])
+    trainer.init_session()
+    trainer.test([valid_target_set])
 
     return
 
