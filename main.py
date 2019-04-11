1daa938199b6ab267760ca93ac3c9cf9df2cab64
ascarrambad <matteoriva@me.com>
2019-04-11 14:53:46 +0200

    - Updated main script for added functionality

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ def cfg():
 
     ### NET SPECS
     # NetSpec = '*CONVLSTM!2-8_*DP_CONV16r!2-1_*MP!2-2_*DP_CONV32r!2-1_*MP!2-2_*FLATFEAT!3_FC128t-51t'
-    NetSpec = '*FLATFEAT!2-1_CONV32r!2-1_*MP!2-2_CONV64r!2-1_*MP!2-2_*DP_*FLATFEAT!3_*ORESHAPE_LSTM128t!0_FC51t'
-    AdvSpec = '*GRADFLIP(Reversal)_FC(DomainFC)64t-3t'
-    AdvFactors = '1-1.0'
+    # NetSpec = '*FLATFEAT!2-1_CONV32r!2-1_*MP!2-2_CONV64r!2-1_*MP!2-2_*DP_*FLATFEAT!3_*ORESHAPE_LSTM128t!0_FC51t'
+    NetSpec = '*FLATFEAT!2-1_*FLATFEAT!2_FC128t_*DP_FC128t_*DP_*ORESHAPE_*LSTM!128-0_*MASKSEQ_*ADVSPLIT_FC128t-51i'
+    AdvSpec = '*GRADFLIP_FC128t-3i'
     ObservedGrads = '' #separate by _
 
     # NET TRAINING
@@ -44,12 +44,11 @@ def cfg():
     BatchSize = 64 # MULTIPLIED BY 2 (source and target)
     LearnRate = 0.001
     InitStd = 0.1
-    StopCondition = 'EarlyStopOnSourceDev'
-    EarlyStoppingPatience = 30
+    EarlyStoppingCondition = 3 # SourceValid
+    EarlyStoppingPatience = 10
 
     OutDir = 'TEST.outdir'
     TensorboardDir = './tensorboard'
-    Remark = ''
 
 ################################################################################
 #################################### SCRIPT ####################################
@@ -64,7 +63,7 @@ def main(
         # NN settings
         NetSpec,AdvSpec,
         # Training settings
-        BatchSize,LearnRate,MaxEpochs,StopCondition,EarlyStoppingPatience,
+        BatchSize,LearnRate,MaxEpochs,EarlyStoppingCondition,EarlyStoppingPatience,
         # Extra settings
         ObservedGrads,OutDir,TensorboardDir,_config
         ):
@@ -78,45 +77,33 @@ def main(
         print('Error %s when making output dir - ignoring' % str(e))
 
     # Data Loader
-    data_loader = Data.Loader(('Source', SourceSpeakers),
-                            ('Target', TargetSpeakers),
-                            ('Extra', ExtraSpeakers))
-
-    # Dataset Specifier
-    train_type = Data.SetType('train')
-    valid_type = Data.SetType('valid')
-    test_type = Data.SetType('test')
+    data_loader = Data.Loader((Data.DomainType.SOURCE, SourceSpeakers),
+                            (Data.DomainType.TARGET, TargetSpeakers),
+                            (Data.DomainType.EXTRA, ExtraSpeakers))
 
     # Load data
-    train_data, _ = data_loader.load_data(train_type, WordsPerSpeaker, VideoNorm, add_channel=True)
-    valid_data, _ = data_loader.load_data(valid_type, WordsPerSpeaker, VideoNorm, add_channel=True)
-    test_data, feature_size = data_loader.load_data(test_type, WordsPerSpeaker, VideoNorm, add_channel=True)
+    train_data, _ = data_loader.load_data(Data.SetType.TRAIN, WordsPerSpeaker, VideoNorm, add_channel=False)
+    valid_data, _ = data_loader.load_data(Data.SetType.VALID, WordsPerSpeaker, VideoNorm, add_channel=False)
+    test_data, feature_size = data_loader.load_data(Data.SetType.TEST, WordsPerSpeaker, VideoNorm, add_channel=False)
 
     # Create source & target datasets for all domain types
-    train_source_set = Data.Set(train_data['Source'], BatchSize, Shuffle)
-    train_target_set = Data.Set(train_data['Target'], BatchSize, Shuffle)
+    train_source_set = Data.Set(train_data[Data.DomainType.SOURCE], BatchSize, Shuffle)
+    train_target_set = Data.Set(train_data[Data.DomainType.TARGET], BatchSize, Shuffle)
 
-    valid_source_set = Data.Set(valid_data['Source'], BatchSize, Shuffle)
-    valid_target_set = Data.Set(valid_data['Target'], BatchSize, Shuffle)
-    valid_extra_set = Data.Set(valid_data['Extra'], BatchSize, Shuffle)
+    valid_source_set = Data.Set(valid_data[Data.DomainType.SOURCE], BatchSize, Shuffle)
+    valid_target_set = Data.Set(valid_data[Data.DomainType.TARGET], BatchSize, Shuffle)
+    valid_extra_set = Data.Set(valid_data[Data.DomainType.EXTRA], BatchSize, Shuffle)
 
-    test_source_set = Data.Set(test_data['Source'], BatchSize, Shuffle)
-    test_target_set = Data.Set(test_data['Target'], BatchSize, Shuffle)
-    test_extra_set = Data.Set(test_data['Extra'], BatchSize, Shuffle)
+    test_source_set = Data.Set(test_data[Data.DomainType.SOURCE], BatchSize, Shuffle)
+    test_target_set = Data.Set(test_data[Data.DomainType.TARGET], BatchSize, Shuffle)
+    test_extra_set = Data.Set(test_data[Data.DomainType.EXTRA], BatchSize, Shuffle)
 
     # Model Builder
-    import pdb; pdb.set_trace()  # breakpoint 0018a623 //
     builder = Model.Builder(InitStd)
-
-    # data_type = tf.float32#tf.as_dtype(train_source_set.data_dtype)
-    # data_shape = (64, 50, 80, 40, 1)#train_source_set.data_shape
-
-    # target_type = tf.int64#tf.as_dtype(train_source_set.target_dtype)
-    # target_shape = (None)#train_source_set.target_shape
 
     # Adding placeholders for data
     builder.add_placeholder(train_source_set.data_dtype, train_source_set.data_shape, 'Sequences')
-    builder.add_placeholder(tf.int64, [None], 'SeqLengths')
+    builder.add_placeholder(tf.int32, [None], 'SeqLengths')
     builder.add_placeholder(train_source_set.target_dtype, train_source_set.target_shape, 'WordTrgs')
     builder.add_placeholder(train_source_set.domain_dtype, train_source_set.domain_shape, 'DomainTrgs')
     builder.add_placeholder(tf.float32, [], 'Lambda')
@@ -124,17 +111,17 @@ def main(
 
     # Create network
     builder.add_main_specification(NetSpec, 'Sequences', 'WordTrgs')
-    # builder.add_specification(AdvSpec, 'Adv/Input', 'DomainTrgs')
-    import pdb; pdb.set_trace()  # breakpoint 308fdf86 //
+    builder.add_specification(AdvSpec, 'ADVSPLIT-9/Input', 'DomainTrgs')
     builder.build_model()
 
-    import pdb; pdb.set_trace()  # breakpoint 69079128 //
+    stopping_type = Model.StoppingType(EarlyStoppingCondition)
     trainer = Model.Trainer(MaxEpochs, LearnRate, builder.graph_specs, builder.placeholders, TensorboardDir)
     trainer.init_session()
-    trainer.train([train_source_set], [valid_source_set])
-    trainer.init_session()
-    trainer.test([valid_target_set])
+    trainer.train(train_sets=[train_source_set, train_target_set],
+                  valid_sets=[valid_source_set, valid_target_set, valid_extra_set],
+                  stopping_type=stopping_type,
+                  stopping_patience=EarlyStoppingPatience)
 
-    return
+
 
 
