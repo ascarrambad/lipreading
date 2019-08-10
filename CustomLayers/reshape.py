47cae88712d27f9d52ff23d736698f2f5bf15e8d
ascarrambad <matteoriva@me.com>
2019-08-10 15:01:29 +0200

    - Minor fix

--- CustomLayers/reshape.py
+++ CustomLayers/reshape.py
@@ -1,7 +1,7 @@
 
 import tensorflow as tf
 
-def _reshape(in_tensor):
+def reshape(in_tensor):
     shape = tf.shape(tf.get_default_graph().get_tensor_by_name('Inputs/MotFrames:0'), name='OrigShape')
     new_shape = tf.concat([shape[:2], tf.shape(in_tensor)[1:]], axis=0, name='NewShape')
 
